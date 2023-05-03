50802de35fff04b3d564f73a27c8d366e0cb907f
Pritam Talukdar <ptalukdar@deloitte.com>
2023-05-03 16:37:39 +0530

    Top bar

--- src/components/Layout/TopBar/TopBar.tsx
+++ src/components/Layout/TopBar/TopBar.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import apps_logo from '../../../../public/images/icons/apps_logo.svg';
+import Image from 'next/image';
 export default function TopBar() {
   return (
-    <div>
-      Top header
+    <div className='bg-black text-white h-10 flex justify-between items-center p-4'>
+      <div>
+        <strong>Logistics orchestration </strong>
+        | TLS
+      </div>
+      <div className='flex justify-between w-20'>
+        <div>
+          <Image src={apps_logo} alt="" />
+        </div>
+        <div>
+          <Image src={apps_logo} alt="" />
+        </div>
+      </div>
     </div>
   )
 }
