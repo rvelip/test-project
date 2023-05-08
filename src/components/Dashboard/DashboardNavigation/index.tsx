90057f94137bf23f9216855f3951a24501d5a3ec
Velip <rvelip@deloitte.com>
2023-05-08 17:55:57 +0530

    header changes

--- src/components/Dashboard/DashboardNavigation/index.tsx
+++ src/components/Dashboard/DashboardNavigation/index.tsx
@@ -5,12 +5,12 @@ export default function DashboardNavigation() {
   return (
     <div className='grid grid-cols-4 h-12 border-b-2 border-grey font-toyota text-sm'>
       <div className='pl-4 border-b-2 border-black flex items-center'>
-        <div>Stall Dashboard</div>
+        <div className='font-semibold'>Stall Dashboard</div>
       </div>
-      <div className='pl-4 bg-black text-white flex items-center'>
+      <div className='pl-4 bg-black text-white flex items-center font-semibold'>
         Stall Lorem
       </div>
-      <div className='pl-4 bg-black text-white flex items-center '>
+      <div className='pl-4 bg-black text-white flex items-center font-semibold '>
         Stall Ipsum
       </div>
     </div>
